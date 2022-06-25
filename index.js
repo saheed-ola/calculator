2a4118ca56c1054bfe4d3c5af8ddc1cae5e1673c
saheed-ola <abubakar.saheed50@gmail.com>
2022-06-25 23:29:07 +0100

    Building calculator using javascript

---
+++ index.js
@@ -0,0 +1,7 @@
+    var MyName = ('Abubakar saheed olamide')
+    var MyHeight = ('5 ft')
+    var MyCountry = ('Nigeria')
+
+    document.write('My name is '+ MyName)
+    document.write('<br> My height '+ MyHeight)
+    document.write("<br>I am from "+ MyCountry)
